098081925fd79a595a37a458049be279c4ee10f5
Tim Thayne <thayneti@byui.edu>
2017-10-04 17:02:57 -0600

    Periodic check-in

--- MegaDesk/SearchQuotes.cs
+++ MegaDesk/SearchQuotes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,63 @@ namespace MegaDesk
         public SearchQuotes(Form mainMenu)
         {
             InitializeComponent();
+
+            // assign private variables
             _mainMenu = mainMenu;
+
+            // set default state
+            comboSurfaceMaterial.SelectedIndex = 0;
+            loadGrid();
         }
 
         private void SearchQuotes_FormClosed(object sender, FormClosedEventArgs e)
         {
             _mainMenu.Show();
+        }
+
+        private void comboSurfaceMaterial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            if (combo.SelectedIndex == 0)
+            {
+                // reset grid
+                loadGrid();
+            }
+            else
+            {
+                // search grid
+                loadGrid(combo.SelectedItem.ToString());
+            }
+        }
+
+        private void loadGrid()
+        {
+            dataGridView1.Rows.Clear();
+
+            string[] deskQuotes = File.ReadAllLines(@"quotes.txt");
+
+            foreach (string deskQuote in deskQuotes)
+            {
+                string[] arrRow = deskQuote.Split(new char[] { ',' });
+                dataGridView1.Rows.Add(arrRow);
+            }
+        }
+
+        private void loadGrid(string searchTerm)
+        {
+            dataGridView1.Rows.Clear();
+
+            string[] deskQuotes = File.ReadAllLines(@"quotes.txt");
+
+            foreach (string deskQuote in deskQuotes)
+            {
+                if (deskQuote.Contains(searchTerm))
+                {
+                    string[] arrRow = deskQuote.Split(new char[] { ',' });
+                    dataGridView1.Rows.Add(arrRow);
+                }
+            }
         }
     }
 }
